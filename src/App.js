98149bfc0f3868b1eefbe3379e140f9f93533bc7
Whitestone2019 <whitedevhub@whitestones.co.in>
2025-05-22 17:18:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the LOS</h1>
+      <h2>Loan Origination System</h2>
+      {/* <div className="mb-6 text-left">
+        <h1 className="text-3xl md:text-4xl font-bold text-gray-800 mb-2">Welcome to the <span style={{color:"#8204FF"}}>LOS</span> </h1>
+        <p className="text-gray-600 text-base">Loan Origination System</p>
+            <h1 className="text-3xl md:text-4xl font-bold text-gray-800 mb-2">Login to your <span style={{color:"#8204FF"}}>Account</span> </h1>
+            <p className="text-gray-600 text-base">See what is going on with your business</p>
+      </div> */}
+    </div>
+
+  );
+}
+
+export default App;
