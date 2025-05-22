117e0f07ba6a6b107be5199f8c3b94b4436fdbdf
Harish Nagure <nagureharish@gmail.com>
2025-05-22 15:54:40 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to the LOS</h1>
+      <h2>Loan Origination System</h2>
+      <div className="mb-6 text-left">
+        <h1 className="text-3xl md:text-4xl font-bold text-gray-800 mb-2">Welcome to the <span style={{color:"#8204FF"}}>LOS</span> </h1>
+        <p className="text-gray-100 text-base">Loan Origination System</p>
+            <h1 className="text-8xl md:text-8xl mb-2">Login to your <span style={{color:"#8204FF"}}>Account</span> </h1>
+            <p className="text-gray-600 text-base">See what is going on with your business</p>
+      </div> 
     </div>
   );
 }
