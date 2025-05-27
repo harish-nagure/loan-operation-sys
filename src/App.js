933dcd8278ecb7e9a2f5663964e890647a03bf9a
Harish Nagure <nagureharish@gmail.com>
2025-05-27 17:52:06 +0530

    Dashboard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -23,23 +22,6 @@ function App() {
         </Routes>
       </Router>
     
-=======
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to the LOS</h1>
-      <h2>Loan Origination System</h2>
-      {/* <div className="mb-6 text-left">
-        <h1 className="text-3xl md:text-4xl font-bold text-gray-800 mb-2">Welcome to the <span style={{color:"#8204FF"}}>LOS</span> </h1>
-        <p className="text-gray-600 text-base">Loan Origination System</p>
-            <h1 className="text-3xl md:text-4xl font-bold text-gray-800 mb-2">Login to your <span style={{color:"#8204FF"}}>Account</span> </h1>
-            <p className="text-gray-600 text-base">See what is going on with your business</p>
-      </div> */}
-    </div>
-
->>>>>>> 98149bfc0f3868b1eefbe3379e140f9f93533bc7
   );
 }
 
